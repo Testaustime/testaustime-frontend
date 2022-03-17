94a8f0e2bc63ab1eea13c845f6f49f05326a179b
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-03-17 21:15:10 +0200

    Fix indentation

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_API || ""
+  baseURL: process.env.REACT_APP_BASE_API || ""
 });
 
 export { instance as axiosInstance };
