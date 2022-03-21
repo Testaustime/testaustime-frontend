47824f65844d3e7ba66b3a0a35d60c946e67a187
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-03-21 20:26:33 +0200

    Add tests for getting the top languages

--- src/components/TopLanguages/TopLanguages.tsx
+++ src/components/TopLanguages/TopLanguages.tsx
@@ -1,26 +1,17 @@
 import { List } from "@mantine/core";
 import { ActivityDataEntry } from "../../hooks/useActivityData";
-import { isStringNull } from "../../utils/stringUtils";
 import { prettifyProgrammingLanguageName } from "../../utils/programmingLanguagesUtils";
 import { prettyDuration } from "../../utils/dateUtils";
-import { groupBy, sumBy } from "../../utils/arrayUtils";
+import { getAllTimeTopLanguages } from "../../lib/topLanguagesStatistics/topLanguagesStatistics";
 
 export interface TopLanguagesProps {
   entries: ActivityDataEntry[]
 }
 
-const getAllTimeTopLanguages = (entries: ActivityDataEntry[]): { language?: string, totalSeconds: number }[] => {
-  const byLanguage = groupBy(entries, entry => entry.language);
-  return Object.keys(byLanguage).map(language => ({
-    language: isStringNull(language) ? undefined : language,
-    totalSeconds: sumBy(byLanguage[language], entry => entry.duration)
-  })).sort((a, b) => b.totalSeconds - a.totalSeconds);
-};
-
 export const TopLanguages = ({ entries }: TopLanguagesProps) => {
   return <List type="ordered" withPadding>
     {getAllTimeTopLanguages(entries).map(l => <List.Item key={l.language || "null"}>
-      {prettifyProgrammingLanguageName(l.language) || <i>Unknown</i>}: {prettyDuration(l.totalSeconds)}
+      {prettifyProgrammingLanguageName(l.language) || <i>Unknown</i>}: {prettyDuration(l.duration)}
     </List.Item>)}
   </List>;
 };
---
+++ src/lib/topLanguagesStatistics/topLanguagesStatistics.ts
@@ -0,0 +1,15 @@
+import { groupBy, sumBy } from "../../utils/arrayUtils";
+import { isStringNull } from "../../utils/stringUtils";
+
+export interface TopLanguagesRequirement {
+  language?: string
+  duration: number
+}
+
+export const getAllTimeTopLanguages = (entries: TopLanguagesRequirement[]) => {
+  const byLanguage = groupBy(entries, entry => entry.language);
+  return Object.keys(byLanguage).map(language => ({
+    language: isStringNull(language) ? undefined : language,
+    duration: sumBy(byLanguage[language], entry => entry.duration)
+  })).sort((a, b) => b.duration - a.duration);
+};
