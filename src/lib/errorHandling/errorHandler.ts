d819544753f4e33f8515cf400661d93c6e2d9512
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-03-23 21:21:23 +0200

    Show notification when rate limited, fixes #5

--- src/lib/errorHandling/errorHandler.ts
+++ src/lib/errorHandling/errorHandler.ts
@@ -4,6 +4,10 @@ export const getErrorMessage = (err: unknown) => {
   if (!axios.isAxiosError(err)) throw err;
   if (!err.response || !err.response.data) throw err;
 
+  if (err.response.status === 429) {
+    return "You are sending too many requests. Slow down";
+  }
+
   const errorResponseData = err.response.data;
   if (typeof errorResponseData === "object" && "error" in errorResponseData && typeof errorResponseData.error === "string") {
     // Return whatever message was returned from the API
