1659a4a99bd3006f45585bf8c20c6d152c1693df
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-04-06 20:16:11 +0300

    Add more stops to calculateTickValues, fixes #29

--- src/utils/chartUtils.ts
+++ src/utils/chartUtils.ts
@@ -1,3 +1,5 @@
+import { lerp } from "./mathUtils";
+
 export const calculateTickValues = (maxDurationSeconds: number) => {
   // If maxDurationSeconds is less than 1 hour, split into 10 minute chunks
   if (maxDurationSeconds <= 3600) {
@@ -24,5 +26,36 @@ export const calculateTickValues = (maxDurationSeconds: number) => {
     return Array(Math.ceil(maxDurationSeconds / 14400) + 1).fill(0).map((_, idx) => idx * 14400);
   }
 
-  throw new Error(`maxDurationSeconds (=${maxDurationSeconds}) exceeded the amount of seconds in 24 hours (=86400).`);
+  // If maxDurationSeconds is less than 50 hours, split into 8 hour chunks
+  if (maxDurationSeconds <= 180000) {
+    return Array(Math.ceil(maxDurationSeconds / 28800) + 1).fill(0).map((_, idx) => idx * 28800);
+  }
+
+  // If maxDurationSeconds is less than 100 hours, split into 15 hour chunks
+  if (maxDurationSeconds <= 360000) {
+    return Array(Math.ceil(maxDurationSeconds / 54000) + 1).fill(0).map((_, idx) => idx * 54000);
+  }
+
+  // If maxDurationSeconds is less than 200 hours, split into 30 hour chunks
+  if (maxDurationSeconds <= 720000) {
+    return Array(Math.ceil(maxDurationSeconds / 108000) + 1).fill(0).map((_, idx) => idx * 108000);
+  }
+
+  // If maxDurationSeconds is less than 400 hours, split into 50 hour chunks
+  if (maxDurationSeconds <= 1440000) {
+    return Array(Math.ceil(maxDurationSeconds / 180000) + 1).fill(0).map((_, idx) => idx * 180000);
+  }
+
+  // If maxDurationSeconds is less than 800 hours, split into 100 hour chunks
+  if (maxDurationSeconds <= 2880000) {
+    return Array(Math.ceil(maxDurationSeconds / 360000) + 1).fill(0).map((_, idx) => idx * 360000);
+  }
+
+  // If maxDurationSeconds is less than 1600 hours, split into 200 hour chunks
+  if (maxDurationSeconds <= 5760000) {
+    return Array(Math.ceil(maxDurationSeconds / 720000) + 1).fill(0).map((_, idx) => idx * 720000);
+  }
+
+  // Linearly interpolate values from 0 to maxDurationSeconds with 7 stops
+  return Array(7).fill(0).map((_, idx) => lerp(0, maxDurationSeconds, idx / 6));
 };
