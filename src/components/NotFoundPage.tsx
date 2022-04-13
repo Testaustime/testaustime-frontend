ab8f00743cd7d17aa9e605904e15fdd28001d101
DrVilepis <jarvinenville606@gmail.com>
2022-04-13 10:57:30 +0300

    Remove trailing whitespace in title

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { Text, Title } from "@mantine/core";
 
 export const NotFoundPage = () => {
   return <>
-    <Title>Page not found </Title>
+    <Title>Page not found</Title>
     <Text>Contact the maintainers if you believe this is not intentional</Text>
   </>;
 };
