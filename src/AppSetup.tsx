f606eef59d8d5926e93231766d64caaf2056f88d
Esinko <34211401+Esinko@users.noreply.github.com>
2022-04-30 16:18:14 +0300

    fix: Add divider and DOM for logged-out menu
    Button cannot be a descendant of button.

--- src/AppSetup.tsx
+++ src/AppSetup.tsx
@@ -218,10 +218,11 @@ export const AppSetup = () => {
                       trigger="hover"
                       control={<Button variant="outline" size="lg"><HamburgerMenuIcon markerHeight={27}></HamburgerMenuIcon></Button>}
                     >
-                      <MenuItem onClick={() => { toggleColorScheme(); }}><ThemeToggle label={true}></ThemeToggle></MenuItem>
+                      <div style={{ padding: "10px" }} onClick={() => { toggleColorScheme(); }}><ThemeToggle label={true}></ThemeToggle></div>
                       {
                         !isLoggedIn ? 
                           <>
+                            <Divider />
                             <Menu.Item component={Link} to="/profile">Login</Menu.Item>
                             <Menu.Item color="blue" component={Link} to="/settings">Register</Menu.Item>
                           </>
