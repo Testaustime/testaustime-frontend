5344e2f0a668c9a6f15e5c59305cc8b02e5ddcbb
Trimpsuz <timo.m.j.helin@gmail.com>
2022-05-09 21:18:00 +0300

    Add All time date range

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -13,23 +13,42 @@ import { PerProjectChart } from "./PerProjectChart";
 import { addDays, format, startOfDay } from "date-fns/esm";
 import useAuthentication from "../hooks/UseAuthentication";
 
-type DayRange = "month" | "week"
+type DayRange = "month" | "week" | "all";
 
 const getDayCount = (dayRange: DayRange) => {
+  const entries = useActivityData().map((entry) => ({
+    ...entry
+  }));
+
+  const currentDate = new Date();
+  const firstCodingDay = entries.at(0)?.start_time;
+  const defaultDate = new Date("Mon Mar 14 2022 00:00:00 GMT+0300 (Eastern European Summer Time)");
+
+  const diff = currentDate.getTime() - (firstCodingDay?.getTime() ?? defaultDate.getTime());
+  
+  const diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
   switch (dayRange) {
     case "month":
       return 30;
     case "week":
       return 7;
+    case "all":
+      return diffDays;
   }
 };
 
-const getAllEntriesByDay = (entries: ActivityDataEntry[]): { date: Date, entries: ActivityDataEntry[] }[] => {
-  const byDayDictionary = groupBy(entries, entry => entry.dayStart.getTime());
-  return Object.keys(byDayDictionary).sort((a, b) => Number(b) - Number(a)).map(key => ({
-    date: new Date(Number(key)),
-    entries: byDayDictionary[key].sort((a, b) => b.start_time.getTime() - a.start_time.getTime())
-  }));
+const getAllEntriesByDay = (
+  entries: ActivityDataEntry[]
+): { date: Date; entries: ActivityDataEntry[] }[] => {
+  const byDayDictionary = groupBy(entries, (entry) => entry.dayStart.getTime());
+  return Object.keys(byDayDictionary)
+    .sort((a, b) => Number(b) - Number(a))
+    .map((key) => ({
+      date: new Date(Number(key)),
+      entries: byDayDictionary[key].sort(
+        (a, b) => b.start_time.getTime() - a.start_time.getTime()
+      )
+    }));
 };
 
 export const Dashboard = () => {
@@ -62,20 +81,20 @@ export const Dashboard = () => {
       "@media (max-width: 480px)": {
         width: "100%",
         minWidth: "unset"
-      },
+      }
     },
     segmentControl: {
       marginTop: 25,
       marginBottom: -3,
       "@media (max-width: 480px)": {
-        width: "100%",
-      },
+        width: "100%"
+      }
     }
   }))();
 
-  const entries = useActivityData().map(entry => ({
+  const entries = useActivityData().map((entry) => ({
     ...entry,
-    language: normalizeProgrammingLanguageName(entry.language),
+    language: normalizeProgrammingLanguageName(entry.language)
   }));
 
   const projectNames = entries.reduce<string[]>((acc, entry) => {
@@ -85,11 +104,13 @@ export const Dashboard = () => {
 
   const shouldFilter = selectedProjects.length > 0;
 
-  const filteredEntries = shouldFilter ?
-    entries.filter(entry => selectedProjects.includes(entry.project_name || "Unknown")) :
-    entries;
+  const filteredEntries = shouldFilter
+    ? entries.filter((entry) =>
+      selectedProjects.includes(entry.project_name || "Unknown")
+    )
+    : entries;
 
-  const entriesInRange = filteredEntries.filter(entry => {
+  const entriesInRange = filteredEntries.filter((entry) => {
     const startOfStatisticsRange = startOfDay(addDays(new Date(), -dayCount));
     return entry.start_time.getTime() >= startOfStatisticsRange.getTime();
   });
@@ -98,70 +119,114 @@ export const Dashboard = () => {
 
   const { username } = useAuthentication();
 
-  return <div style={{ width: "100%" }}>
-    <Group style={{ marginBottom: "1rem" }}>
-      <Text>Welcome, <b>{username}</b></Text>
-    </Group>
-    <Title mb={5}>Your statistics</Title>
-    <Group align="end" position="apart" mt={10} mb={30}>
-      {projectNames.length !== 0 ? 
-        <>
+  return (
+    <div style={{ width: "100%" }}>
+      <Group style={{ marginBottom: "1rem" }}>
+        <Text>
+          Welcome, <b>{username}</b>
+        </Text>
+      </Group>
+      <Title mb={5}>Your statistics</Title>
+      <Group align="end" position="apart" mt={10} mb={30}>
+        {projectNames.length !== 0 ? (
+          <>
+            <MultiSelect
+              label="Projects"
+              data={projectNames}
+              value={selectedProjects}
+              className={classes.multiSelect}
+              onChange={(selectedProjectNames: string[]) =>
+                setSelectedProjects(selectedProjectNames)
+              }
+              clearable
+              placeholder="Select a project filter"
+            />
+            <SegmentedControl
+              data={[
+                { label: "Last week", value: "week" },
+                { label: "Last month", value: "month" },
+                { label: "All time", value: "all" }
+              ]}
+              value={statisticsRange}
+              onChange={(value: DayRange) => setStatisticsRange(value)}
+              className={classes.segmentControl}
+            />
+          </>
+        ) : (
           <MultiSelect
             label="Projects"
             data={projectNames}
-            value={selectedProjects}
-            className={classes.multiSelect}
-            onChange={(selectedProjectNames: string[]) => setSelectedProjects(selectedProjectNames)}
-            clearable
-            placeholder="Select a project filter"
+            placeholder="No projects"
+            disabled
           />
-          <SegmentedControl
-            data={[
-              { label: "Last week", value: "week" },
-              { label: "Last month", value: "month" },
-            ]}
-            value={statisticsRange}
-            onChange={(value: DayRange) => setStatisticsRange(value)}
-            className={classes.segmentControl}
-          />
-        </> : <MultiSelect
-          label="Projects"
-          data={projectNames}
-          placeholder="No projects"
-          disabled
-        />}
-    </Group>
-    {entriesInRange.length !== 0 ? 
-      <>
-        <Group className={classes.dataCard}>
-          <Title mt={10} mb={-35} order={2}>Time per day</Title>
-          <DailyCodingTimeChart entries={entriesInRange} dayCount={dayCount} className={classes.dailyCodingTimeChart}/>
-          <Text mt={15} mb={15}>Total time coded in the last {dayCount} days: <b>{prettyDuration(sumBy(entriesInRange, entry => entry.duration))}</b></Text>
-        </Group>
-        <Group className={classes.dataCard}>
-          <Title mt={10} mb={-30} order={2}>Time per latest projects</Title>
-          <PerProjectChart entries={entriesInRange} className={classes.projectCodingChart}/>
-        </Group>
-        <Group direction={isSmallScreen ? "column" : "row"} grow mt={20} mb={20} align="start">
-          <div>
-            <Title order={2}>Languages</Title>
-            <TopLanguages entries={entriesInRange} />
-          </div>
-          <div>
-            <Title order={2}>Projects</Title>
-            <TopProjects entries={entriesInRange} />
-          </div>
-        </Group>
-        <Title order={2} mt={20} mb={5}>Your sessions</Title>
-        <Accordion multiple>
-          {getAllEntriesByDay(entriesInRange).map(d =>
-            <Accordion.Item key={d.date.getTime()} label={<Text size="lg">{format(d.date, "d.M.yyyy")}</Text>}>
-              <DaySessions entries={d.entries} />
-            </Accordion.Item>)}
-        </Accordion>
-      </>
-      :
-      <Text>No programming activity data to display. <a href="/extensions">Install one of the extensions</a> to begin tracking your programming.</Text>
-    }
-  </div>;
+        )}
+      </Group>
+      {entriesInRange.length !== 0 ? (
+        <>
+          <Group className={classes.dataCard}>
+            <Title mt={10} mb={-35} order={2}>
+              Time per day
+            </Title>
+            <DailyCodingTimeChart
+              entries={entriesInRange}
+              dayCount={dayCount}
+              className={classes.dailyCodingTimeChart}
+            />
+            <Text mt={15} mb={15}>
+              Total time coded in the last {dayCount} days:{" "}
+              <b>
+                {prettyDuration(
+                  sumBy(entriesInRange, (entry) => entry.duration)
+                )}
+              </b>
+            </Text>
+          </Group>
+          <Group className={classes.dataCard}>
+            <Title mt={10} mb={-30} order={2}>
+              Time per latest projects
+            </Title>
+            <PerProjectChart
+              entries={entriesInRange}
+              className={classes.projectCodingChart}
+            />
+          </Group>
+          <Group
+            direction={isSmallScreen ? "column" : "row"}
+            grow
+            mt={20}
+            mb={20}
+            align="start"
+          >
+            <div>
+              <Title order={2}>Languages</Title>
+              <TopLanguages entries={entriesInRange} />
+            </div>
+            <div>
+              <Title order={2}>Projects</Title>
+              <TopProjects entries={entriesInRange} />
+            </div>
+          </Group>
+          <Title order={2} mt={20} mb={5}>
+            Your sessions
+          </Title>
+          <Accordion multiple>
+            {getAllEntriesByDay(entriesInRange).map((d) => (
+              <Accordion.Item
+                key={d.date.getTime()}
+                label={<Text size="lg">{format(d.date, "d.M.yyyy")}</Text>}
+              >
+                <DaySessions entries={d.entries} />
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        </>
+      ) : (
+        <Text>
+          No programming activity data to display.{" "}
+          <a href="/extensions">Install one of the extensions</a> to begin
+          tracking your programming.
+        </Text>
+      )}
+    </div>
+  );
 };
