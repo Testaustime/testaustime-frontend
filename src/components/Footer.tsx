b4b0c6ac9e2a10f7083c0752769bb556a6b9c2bc
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-05-12 17:33:18 +0300

    Import Testausserveri logo as a React component, fixes #51

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,55 +1,56 @@
 import { createStyles, Text, Grid } from "@mantine/core";
-import TestausserveriLogo from "../images/testausserveri.svg";
-
+import { ReactComponent as TestausserveriLogo } from "../images/testausserveri.svg";
 const authors = [
   "Luukas Pörtfors",
   "Ville Järvinen",
   "\"Eldemarkki\""
 ];
 
-export const Footer = () => {
-  const { classes } = createStyles(theme => ({
-    container: {
-      marginTop: "100px",
-      width: "100%",
-      display: "flex",
-      justifyContent: "flex-start",
+const useStyles = createStyles(theme => ({
+  container: {
+    marginTop: "100px",
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-start",
+    flexDirection: "column"
+  },
+  footerText: {
+    fontSize: "1.2rem",
+    color: theme.colorScheme === "dark" ? "#bbb" : "#333"
+  },
+  line: {
+    width: "100%",
+    height: "1px",
+    backgroundColor: theme.colorScheme === "dark" ? "#333" : "#ddd"
+  },
+  grid: {
+    "@media (max-width: 495px)": {
       flexDirection: "column"
-    },
-    footerText: {
-      fontSize: "1.2rem",
-      color: theme.colorScheme === "dark" ? "#bbb" : "#333"
-    },
-    line: {
-      width: "100%",
-      height: "1px",
-      backgroundColor: theme.colorScheme === "dark" ? "#333" : "#ddd"
-    },
-    grid: {
-      "@media (max-width: 495px)": {
-        flexDirection: "column"
-      }
-    },
-    gridItem: {
-      "@media (max-width: 495px)": {
-        minWidth: "100%"
-      }
-    },
-    rightAlign: {
-      textAlign: "right",
-      "@media (max-width: 495px)": {
-        textAlign: "left"
-      }
-    },
-    bwLogo: {
-      filter: theme.colorScheme === "dark" ? "invert(1)" : "none",
-      opacity: "0.8",
-      transition: "opacity 0.2s !important",
-      ":hover": {
-        opacity: "1"
-      }
     }
-  }))();
+  },
+  gridItem: {
+    "@media (max-width: 495px)": {
+      minWidth: "100%"
+    }
+  },
+  rightAlign: {
+    textAlign: "right",
+    "@media (max-width: 495px)": {
+      textAlign: "left"
+    }
+  },
+  bwLogo: {
+    fill: theme.colorScheme === "dark" ? "white" : "black",
+    opacity: "0.8",
+    transition: "opacity 0.2s !important",
+    ":hover": {
+      opacity: "1"
+    }
+  }
+}));
+
+export const Footer = () => {
+  const { classes } = useStyles();
 
   const authorsList = `${authors.slice(0, authors.length - 1).join(", ")} and ${authors[authors.length - 1]}`;
 
@@ -58,13 +59,12 @@ export const Footer = () => {
       <span className={classes.line} />
       <Grid justify="space-between" mt="40px" className={classes.grid}>
         <Grid.Col span={4} className={classes.gridItem}>
-          <a href="https://testausserveri.fi" style={{ display: "flex" }}>
-            <img
-              src={TestausserveriLogo}
-              alt="Testausserveri ry"
-              style={{ marginRight: "8px" }}
+          <a href="https://testausserveri.fi">
+            <TestausserveriLogo
+              className={classes.bwLogo}
               height={90}
-              className={classes.bwLogo} />
+              width={155}
+            />
           </a>
           <Text>Supported by Testausserveri ry</Text>
         </Grid.Col>
