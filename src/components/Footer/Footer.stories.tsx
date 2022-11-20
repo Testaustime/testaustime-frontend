5a20502c467e8bc7d0f34b7a1117fb0907423297
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-11-20 17:18:52 +0200

    feat(stories): add stories for Footer

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { withSetup } from "../../../.storybook/withSetup";
+import { Footer } from "./Footer";
+
+export default {
+  title: "Footer",
+  component: Footer,
+  decorators: [withSetup]
+} as Meta;
+
+const Template: StoryFn = () => <Footer />;
+export const Default = Template.bind({});
