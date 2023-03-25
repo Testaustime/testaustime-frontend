89ac327221da545a2b087c63b8975ef69d76e0d5
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-03-25 19:04:18 +0200

    chore(deps): upgrade react-query to version 4

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuthentication } from "./useAuthentication";
 import { User } from "./useAuthentication";
 import { useNavigate } from "react-router";
@@ -10,21 +10,26 @@ export const useAccount = () => {
   const navigate = useNavigate();
 
   const { mutateAsync: changeAccountVisibility } =
-    useMutation("changeUserVisibility", (visibility: boolean) => axios.post("/account/settings", {
-      public_profile: visibility
-    }), {
-      onSuccess: (_, newVisibility) => {
-        queryClient.setQueryData("fetchUser", (old: User | undefined) => {
-          if (!old) throw new Error("User not found");
-          return {
-            ...old,
-            isPublic: newVisibility
-          };
-        });
+    useMutation(
+      ["changeUserVisibility"],
+      (visibility: boolean) => axios.post("/account/settings", {
+        public_profile: visibility
+      }),
+      {
+        onSuccess: (_, newVisibility) => {
+          queryClient.setQueryData(["fetchUser"], (old: User | undefined) => {
+            if (!old) throw new Error("User not found");
+            return {
+              ...old,
+              isPublic: newVisibility
+            };
+          });
+        }
       }
-    });
+    );
 
-  const { mutateAsync: deleteAccount } = useMutation("deleteAccount",
+  const { mutateAsync: deleteAccount } = useMutation(
+    ["deleteAccount"],
     (password: string) => axios.delete("/users/@me/delete", {
       data: {
         username,
@@ -33,11 +38,12 @@ export const useAccount = () => {
     }),
     {
       onSuccess: () => {
-        queryClient.removeQueries("fetchUser");
+        queryClient.removeQueries(["fetchUser"]);
         logOut();
         navigate("/");
       }
-    });
+    }
+  );
 
   return {
     changeAccountVisibility: (visibility: boolean) => changeAccountVisibility(visibility),
