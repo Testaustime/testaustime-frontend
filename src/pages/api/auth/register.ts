2f36255fbe32cf2242784a26973e164d0a86fc08
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-05-18 15:29:59 +0300

    feat: create wrapper for registering

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,38 @@
+import axios, { isAxiosError } from "axios";
+import { NextApiHandler } from "next";
+import { ApiAuthRegisterResponse } from "../../../hooks/useAuthentication";
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method === "POST") {
+    if (process.env.NEXT_PUBLIC_API_URL === undefined) return res.status(500).json({ message: "Missing API URL" });
+
+    try {
+      const response = await axios.post<ApiAuthRegisterResponse>(
+        "/auth/register", req.body,
+        {
+          headers: {
+            "X-Forwarded-For": req.socket.remoteAddress
+          },
+          baseURL: process.env.NEXT_PUBLIC_API_URL
+        });
+      const token = response.data.auth_token;
+      const expiration = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7).toUTCString();
+      res.setHeader("Set-Cookie", `token=${token}; path=/; expires=${expiration}; SameSite=Strict; Secure; HttpOnly`);
+      return res.status(response.status).json(response.data);
+    } catch (e) {
+      if (isAxiosError(e)) {
+        if (!e.response) return res.status(500).json({ message: "Internal server error" });
+        return res.status(e.response.status).json(e.response.data);
+      }
+    }
+
+    // This should never happen
+    console.error("This should never happen");
+    return res.status(500).json({ message: "Internal server error" });
+  }
+  else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+};
+
+export default handler;
