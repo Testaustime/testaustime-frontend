ba881a00f13cbd34337021d09851853449e14215
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-11-18 19:27:18 +0200

    feat: use server actions for regenerating auth token

--- src/app/[locale]/profile/AuthTokenField.tsx
+++ src/app/[locale]/profile/AuthTokenField.tsx
@@ -1,32 +1,21 @@
 "use client";
 
-import axios from "../../../axios";
 import TokenField from "../../../components/TokenField";
-import { getErrorMessage } from "../../../lib/errorHandling/errorHandler";
-
-interface ApiAuthRegenerateResponse {
-  token: string;
-}
+import { regenerateToken } from "./actions";
 
 export const AuthTokenField = ({ token }: { token: string }) => {
-  const regenerateToken = async () => {
-    try {
-      const { data } = await axios.post<ApiAuthRegenerateResponse>(
-        "/auth/regenerate",
-        null,
-      );
-      const newToken = data.token;
-      return newToken;
-    } catch (error) {
-      // eslint-disable-next-line @typescript-eslint/no-throw-literal
-      throw getErrorMessage(error);
-    }
-  };
-
   return (
     <TokenField
       value={token}
-      regenerate={regenerateToken}
+      regenerate={async () => {
+        const result = await regenerateToken();
+        if (typeof result === "object") {
+          // TODO: Show error
+          throw new Error(result.error);
+        }
+
+        return result;
+      }}
       censorable
       revealLength={4}
     />
---
+++ src/app/[locale]/profile/actions.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+
+interface ApiAuthRegenerateResponse {
+  token: string;
+}
+
+export const regenerateToken = async () => {
+  const token = cookies().get("token")?.value;
+
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/auth/regenerate",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  if (!response.ok) {
+    return { error: "Unknown error" as const };
+  }
+
+  const data = (await response.json()) as ApiAuthRegenerateResponse;
+
+  const expiration = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
+
+  cookies().set("token", data.token, {
+    value: data.token,
+    expires: expiration,
+    path: "/",
+    sameSite: "strict",
+    secure: true,
+    httpOnly: true,
+  });
+
+  revalidateTag("users/@me");
+
+  const newToken = data.token;
+  return newToken;
+};
