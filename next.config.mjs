7449042231de84fb23cca49d5035c6f4189271cc
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-12-17 21:48:21 +0200

    chore: change to ESM and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-/* eslint-disable */
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   webpack(config) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     config.module.rules.push({
       test: /\.svg$/i,
       use: ["@svgr/webpack"],
     });
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
