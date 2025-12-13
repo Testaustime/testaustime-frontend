8600a8e86c8ee4add93bfb24de1dbad19847017a
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-12-13 21:56:36 +0200

    fix: fix join leaderboard errors

--- src/components/leaderboard/actions.ts
+++ src/components/leaderboard/actions.ts
@@ -5,14 +5,26 @@ import {
   postRequestWithoutResponse,
   postRequestWithResponse,
 } from "../../api/baseApi";
+import { JoinLeaderboardError } from "../../types";
 
-export const joinLeaderboard = (inviteCode: string) =>
-  postRequestWithResponse<{
+export const joinLeaderboard = async (inviteCode: string) => {
+  const response = await postRequestWithResponse<{
     name: string;
     member_count: number;
   }>("/leaderboards/join", {
     invite: inviteCode,
   });
+
+  if ("error" in response) {
+    if (response.statusCode === 404) {
+      return { error: JoinLeaderboardError.NotFound };
+    } else if (response.statusCode === 409) {
+      return { error: JoinLeaderboardError.AlreadyMember };
+    }
+  }
+
+  return response;
+};
 
 export const leaveLeaderboard = async (leaderboardName: string) => {
   const data = await postRequestWithoutResponse(
