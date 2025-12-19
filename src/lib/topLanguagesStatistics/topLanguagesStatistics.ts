be2fd9efff43ff76161002eb84b8e65f309216cb
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-12-19 19:06:35 +0200

    fix: fix crash when project or language name was __proto__ or other prototype pollution keyword

--- src/lib/topLanguagesStatistics/topLanguagesStatistics.ts
+++ src/lib/topLanguagesStatistics/topLanguagesStatistics.ts
@@ -1,4 +1,4 @@
-import { groupBy, sumBy } from "../../utils/arrayUtils";
+import { sumBy } from "../../utils/arrayUtils";
 import { isStringNull } from "../../utils/stringUtils";
 
 interface TopLanguagesRequirement {
@@ -7,11 +7,14 @@ interface TopLanguagesRequirement {
 }
 
 export const getAllTimeTopLanguages = (entries: TopLanguagesRequirement[]) => {
-  const byLanguage = groupBy(entries, (entry) => entry.language);
+  const byLanguage = Object.groupBy(
+    entries,
+    (entry) => entry.language ?? "undefined",
+  );
   return Object.keys(byLanguage)
     .map((language) => ({
       language: isStringNull(language) ? undefined : language,
-      duration: sumBy(byLanguage[language], (entry) => entry.duration),
+      duration: sumBy(byLanguage[language] ?? [], (entry) => entry.duration),
     }))
     .sort((a, b) => b.duration - a.duration);
 };
--- src/utils/programmingLanguagesUtils.ts
+++ src/utils/programmingLanguagesUtils.ts
@@ -1,81 +1,84 @@
 import { capitalizeFirstLetter, isStringNull } from "./stringUtils";
 
+// Create "synonyms" for the programming language names.
+// The values will be used to get the language's name in `prettifyProgrammingLanguageName`
+const normalizedNames = new Map(
+  Object.entries({
+    cs: "csharp",
+    ts: "typescript",
+    tsx: "typescriptreact",
+    js: "javascript",
+    jsx: "javascriptreact",
+    perl6: "perl",
+    jade: "pug",
+    gitcommit: "git-commit",
+    gitrebase: "git-rebase",
+    shellscript: "sh",
+    make: "makefile",
+  }),
+);
+
 export const normalizeProgrammingLanguageName = (
   name: string | undefined | null,
 ) => {
   if (!name || isStringNull(name)) return null;
-
-  // Create "synonyms" for the programming language names.
-  // The values will be used to get the language's name in `prettifyProgrammingLanguageName`
-  return (
-    {
-      cs: "csharp",
-      ts: "typescript",
-      tsx: "typescriptreact",
-      js: "javascript",
-      jsx: "javascriptreact",
-      perl6: "perl",
-      jade: "pug",
-      gitcommit: "git-commit",
-      gitrebase: "git-rebase",
-      shellscript: "sh",
-      make: "makefile",
-    }[name] || name
-  );
+  return normalizedNames.get(name) || name;
 };
+
+const languageNames = new Map(
+  Object.entries({
+    typescript: "TypeScript",
+    typescriptreact: "TypeScript with React",
+    javascript: "JavaScript",
+    javascriptreact: "JavaScript with React",
+    json: "JSON",
+    html: "HTML",
+    css: "CSS",
+    rust: "Rust",
+    vimwiki: "VimWiki",
+    abap: "ABAP",
+    bat: "Windows Batch",
+    bibtex: "BibTeX",
+    coffeescript: "CoffeeScript",
+    cpp: "C++",
+    csharp: "C#",
+    "cuda-cpp": "CUDA C++",
+    fsharp: "F#",
+    "git-commit": "Git Commit",
+    "git-rebase": "Git Rebase",
+    jsonc: "JSON with Comments",
+    latex: "LaTeX",
+    "objective-c": "Objective-C",
+    "objective-cpp": "Objective-C++",
+    php: "PHP",
+    plaintext: "Plain Text",
+    powershell: "PowerShell",
+    scss: "SCSS",
+    sass: "SASS",
+    shaderlab: "ShaderLab",
+    sh: "Shell Script",
+    sql: "SQL",
+    text: "TeX",
+    vb: "Visual Basic",
+    "vue-html": "Vue HTML",
+    xml: "XML",
+    xsl: "XSL",
+    yaml: "YAML",
+    toml: "TOML",
+    cabalconfig: "Cabal Config",
+    zsh: "ZSH",
+    nginx: "NGINX",
+    sshconfig: "SSH Config",
+    conf: "Config",
+    dockercompose: "Docker Compose",
+    mdx: "Markdown Extended",
+  }),
+);
 
 export const prettifyProgrammingLanguageName = (
   name: string | undefined | null,
 ) => {
   if (!name || isStringNull(name)) return null;
 
-  return (
-    {
-      typescript: "TypeScript",
-      typescriptreact: "TypeScript with React",
-      javascript: "JavaScript",
-      javascriptreact: "JavaScript with React",
-      json: "JSON",
-      html: "HTML",
-      css: "CSS",
-      rust: "Rust",
-      vimwiki: "VimWiki",
-      abap: "ABAP",
-      bat: "Windows Batch",
-      bibtex: "BibTeX",
-      coffeescript: "CoffeeScript",
-      cpp: "C++",
-      csharp: "C#",
-      "cuda-cpp": "CUDA C++",
-      fsharp: "F#",
-      "git-commit": "Git Commit",
-      "git-rebase": "Git Rebase",
-      jsonc: "JSON with Comments",
-      latex: "LaTeX",
-      "objective-c": "Objective-C",
-      "objective-cpp": "Objective-C++",
-      php: "PHP",
-      plaintext: "Plain Text",
-      powershell: "PowerShell",
-      scss: "SCSS",
-      sass: "SASS",
-      shaderlab: "ShaderLab",
-      sh: "Shell Script",
-      sql: "SQL",
-      text: "TeX",
-      vb: "Visual Basic",
-      "vue-html": "Vue HTML",
-      xml: "XML",
-      xsl: "XSL",
-      yaml: "YAML",
-      toml: "TOML",
-      cabalconfig: "Cabal Config",
-      zsh: "ZSH",
-      nginx: "NGINX",
-      sshconfig: "SSH Config",
-      conf: "Config",
-      dockercompose: "Docker Compose",
-      mdx: "Markdown Extended",
-    }[name] || capitalizeFirstLetter(name)
-  );
+  return languageNames.get(name) || capitalizeFirstLetter(name);
 };
